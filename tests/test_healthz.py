778ba1456dab57ecb54ba5e0e78045083a288a7f
AndyStar <andy_star@ukr.net>
2025-11-06 17:26:59 +0200

    Tests updated to avoid merge conflict

--- tests/test_healthz.py
+++ tests/test_healthz.py
@@ -1,6 +1,9 @@
 import importlib
 import pytest
-from httpx import AsyncClient, ASGITransport
+from httpx import (
+    AsyncClient,
+    ASGITransport)
+
 
 from app.main import app
 from app.core import config as config_module
